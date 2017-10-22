4ab0bd6df66d0fa00574f64a56f1ff9eee015898
Brian Watt <bwatt@1fifoto.com>
2017-10-22 14:55:09 -0500

    Added check for submit and file

--- src/public/index.php
+++ src/public/index.php
@@ -1,27 +1,36 @@
 <?php
-$file = '';
-if ($file = $_REQUEST['file']) {
-  echo $file;
+if (isset($_POST["submit"])) {
+    if ($file=isset($_FILES["file"])) {
+        echo "File $file selected <br />";
+    } else {
+        echo "No file selected <br />";
+    }
 }
 ?>
 
 <html>
-  <body>
-    <h1>Signer Cams</h1>
-    <table width="600">
-      <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" enctype="multipart/form-data">
-        <tr>
-          <td width="20%">Select file</td>
-          <td width="80%"><input type="file" name="file" id="file" /></td>
-        </tr>
-        <tr>
-          <td>Submit</td>
-          <td><input type="submit" name="submit" /></td>
-        </tr>
-      </form>
-    </table>
-  </body>
-  <footer>
-    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
-  </footer>
+<body>
+	<h1>Signer Cams</h1>
+	<table width="600">
+		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post"
+			enctype="multipart/form-data">
+			<tr>
+				<td width="20%">Select file</td>
+				<td width="80%"><input type="file" name="file" id="file" /></td>
+			</tr>
+			<tr>
+				<td>Submit</td>
+				<td><input type="submit" name="submit" /></td>
+			</tr>
+		</form>
+	</table>
+</body>
+<footer>
+	<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img
+		alt="Creative Commons License" style="border-width: 0"
+		src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This
+	work is licensed under a <a rel="license"
+		href="http://creativecommons.org/licenses/by/4.0/">Creative Commons
+		Attribution 4.0 International License</a>.
+</footer>
 </html>
